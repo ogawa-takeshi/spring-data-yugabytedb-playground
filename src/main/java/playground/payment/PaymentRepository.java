b337a2b0015b0e5f48a69c578b24c3b6eaaa246f
OGAWA, Takeshi <ogawa@tagbangers.co.jp>
2022-03-29 14:24:55 +0900

    Scaffolding

--- src/main/java/playground/payment/PaymentRepository.java
+++ src/main/java/playground/payment/PaymentRepository.java
@@ -1,9 +1,11 @@
 package playground.payment;
 
 import com.yugabyte.data.jdbc.repository.YsqlRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PaymentRepository extends YsqlRepository<Payment, Long> {
 
+    @Transactional(readOnly = true)
     Iterable<Payment> findAllByGeoPartition(String geoPartition);
 
 }
